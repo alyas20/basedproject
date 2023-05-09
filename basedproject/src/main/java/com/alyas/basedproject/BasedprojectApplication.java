7b2e2a3f9c8d2a06d72424b00d9f5e7b5e0f3c65
DESKTOP-T13L522\alyas <alyasamsyar@yahoo.com>
2023-05-10 02:06:31 +0800

    init-project

---
+++ basedproject/src/main/java/com/alyas/basedproject/BasedprojectApplication.java
@@ -0,0 +1,13 @@
+package com.alyas.basedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasedprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasedprojectApplication.class, args);
+	}
+
+}
